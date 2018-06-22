807cc37d5d6f47c19f47d17f0f46df64a5b206e6
zynkn <ywnwalone@naver.com>
2018-06-22 19:22:48 +1000

    init edits

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 - 이 프로젝트는 Fireact 프로젝트를 시작하기 위한 Starter Kit 입니다.
 
 - 이 프로젝트의 구조는 [velopert](https://github.com/velopert) 님의 것을 참고하였습니다.
+
+- The latest update 6/22/2018
--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      Fireact initial setting
+    <div style={{ height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <h1><span role="img" aria-label="fire">🔥</span>Fireact<span role="img" aria-label="atom">⚛️</span></h1>
     </div>
   );
 };
